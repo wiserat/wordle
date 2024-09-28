49af45d8ab467eb2d12113733049d76efc8f331a
jonastenora <jonas.tenora@gmail.com>
2024-09-28 21:46:58 +0200

    initial

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,50 +1,139 @@
 ﻿using System;
+using System.IO;
 
 namespace ConsoleApp1
 {
+    
+    public class CharWithColor
+    {
+        public char Character { get; set; }
+        public ConsoleColor Color { get; set; }
+
+        public CharWithColor(char character, ConsoleColor color)
+        {
+            Character = character;
+            Color = color;
+        }
+    }
+    
     class Program
     {
+
+        static void End()
+        {
+            Console.Write("End game? [y/n]: ");
+            string input = Console.ReadLine();
+            if (input.ToLower() == "y")
+            {
+                Environment.Exit(0);
+            }
+        }
+        
         static void Main(string[] args)
         {
-            string word = "array";
-            Console.WriteLine("Wordle! 6 tries to guess the word, type a 5char word");
-
-            for (int i = 0; i <= 6; i++)
+            string[] lines =
+                File.ReadAllLines("/Users/jonastenora/code/csharp/wordle/ConsoleApp1/ConsoleApp1/words.txt");
+            Random random = new Random();
+            
+            while (true)
             {
-                string guess = "";
-                while (guess.Length != 5)
-                {
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.WriteLine();
-                    Console.Write("Guess: ");
-                    guess = Console.ReadLine();
-                    if (guess == word)
-                    {
-                        Console.Write("Correctly guessed: ");
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.Write(guess.ToUpper());
-                        Environment.Exit(0);
-                    }
-                }
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Wordle! 6 tries to guess the word, type a 5-char word".ToUpper());
+                Console.ResetColor();
+                Console.WriteLine();
+                
+                int randomIndex = random.Next(lines.Length);
+                string word = lines[randomIndex];
+                List<CharWithColor[]> words = new List<CharWithColor[]>();
 
-                for (int j = 0; j < guess.Length; j++)
+                int numberOfTries = 6;
+                while (true)
                 {
-                    if (guess[j] == word[j])
+                    Console.Write("How hard do you want the game to be? (amount of guesses): ");
+                    string numberOfTriesString = Console.ReadLine();
+                    if (int.TryParse(numberOfTriesString, out int numberOfTriesInt) && numberOfTriesInt > 0)
                     {
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.Write(guess[j]);
-                    }
-                    else if (word.Contains(guess[j]))
-                    {
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.Write(guess[j]);
+                        numberOfTries = numberOfTriesInt;
+                        break;
                     }
                     else
                     {
-                        Console.ForegroundColor = ConsoleColor.White;
-                        Console.Write(guess[j]);
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Different input expected!");
+                        Console.ResetColor();
                     }
+
                 }
+                
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine($"Okay, {numberOfTries} it is. Enjoy the game!");
+                Console.ResetColor();
+
+                for (int i = 0; i < numberOfTries; i++)
+                {
+                    string guess = "";
+                    bool firstTimeGuessing = true;
+                    Console.WriteLine();
+                    while (guess.Length != 5 || !lines.Contains(guess))
+                    {
+                        if (!firstTimeGuessing)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Different input expected!");
+                            Console.ResetColor();
+                        }
+                        firstTimeGuessing = false;
+                        Console.ResetColor();
+                        Console.Write($"Guess {i + 1}: ");
+                        guess = Console.ReadLine();
+                        if (guess == word)
+                        {
+                            Console.Write("Correctly guessed: ");
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write(guess.ToUpper());
+                            End();
+                        }
+                    }
+
+                    foreach (CharWithColor[] displayWord in words)
+                    {
+                        foreach (var ch in displayWord)
+                        {
+                            Console.ForegroundColor = ch.Color;
+                            Console.Write(ch.Character);
+                        }
+                        Console.WriteLine();
+                    }
+                    CharWithColor[] guessedWord = new CharWithColor[5];
+                    for (int j = 0; j < guess.Length; j++)
+                    {
+                        if (guess[j] == word[j])
+                        {
+                            guessedWord[j] = new CharWithColor(guess[j], ConsoleColor.Green);
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write(guess[j]);
+                        }
+                        else if (word.Contains(guess[j]))
+                        {
+                            guessedWord[j] = new CharWithColor(guess[j], ConsoleColor.Yellow);
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.Write(guess[j]);
+                        }
+                        else
+                        {
+                            guessedWord[j] = new CharWithColor(guess[j], ConsoleColor.White);
+                            Console.ResetColor();
+                            Console.Write(guess[j]);
+                        }
+                    }
+                    words.Add(guessedWord);
+
+                    Console.WriteLine();
+                }
+                Console.ResetColor();
+                Console.WriteLine();
+                Console.WriteLine($"You loosed! Correct word: {word}");
+                End();
             }
         }
     }
